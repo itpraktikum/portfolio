bf096bf4d296d50c5ceeb4b57ce9d81c83cc6f74
Sarvarbek <serkinov393@gmail.com>
2023-01-13 00:05:32 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const laoder = document.querySelector(".loader"),
           header = document.querySelector(".header"),
+          openNavBtn = document.querySelector("#openNav"),
+          closeNavBtn = document.querySelector("#closeNav"),
+          navbar = document.querySelector(".nav-link"),
+          openCont = document.querySelector(".btn"),
+          closeCont = document.querySelector(".open-cont"),
+          contactbox = document.querySelector(".contact"),
           navLinks = document.querySelectorAll(".link");
 
     // The started loader
@@ -36,5 +42,38 @@ window.addEventListener("DOMContentLoaded", () => {
                 laoder.style.display = "none";
             },1000)
         })
+    });
+
+    // The started openNav or closeNav
+
+    openNavBtn.addEventListener("click", () => {
+        navbar.style.display = "flex";
+    })    
+
+    closeNavBtn.addEventListener("click", () => {
+        navbar.style.display = "none";
+    });
+
+    // The started open or close contactbox
+
+    openCont.addEventListener("click", () => {
+        setTimeout(() => {
+            laoder.style.display = "flex";
+        },100);
+        setTimeout(() => {
+            laoder.style.display = "none";
+        },1000)
+        contactbox.style.display = "flex";
     })
+
+    closeCont.addEventListener("click", () => {
+        setTimeout(() => {
+            laoder.style.display = "flex";
+        },100);
+        setTimeout(() => {
+            laoder.style.display = "none";
+        },1000)
+        contactbox.style.display = "none"
+    })
+
 })
